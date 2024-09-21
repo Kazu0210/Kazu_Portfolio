79cf2be40c18e712b545eeb956e1a9be7ac7d7ac
Kazu0210 <clffrdkyl@gmail.com>
2024-09-21 17:37:38 +0800

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Kzu_Portfolio/'  // Make sure this matches your repo name and has no leading space
+  base: '/Kazu_Portfolio/'  // Make sure this matches your repo name and has no leading space
 })
