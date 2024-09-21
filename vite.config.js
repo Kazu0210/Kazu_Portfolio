b330ae0c613dd99c39d46974ef7cf1e13c64d010
Kazu0210 <clffrdkyl@gmail.com>
2024-09-21 17:28:02 +0800

    add: initial files

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Kzu_Portfolio/'  // Make sure this matches your repo name and has no leading space
+})
